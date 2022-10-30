e8bc827193b81668a8917d316e45cc2b565e379b
Michael Combrink <u19009209@up.ac.za>
2022-10-30 14:51:37 +0200

    Add project files.

---
+++ u19009209_HW06/Models/Chart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace u19009209_HW06.Models
+{
+    public class Chart
+    {
+        public string[] labels { get; set; }
+        public List<Datasets> datasets { get; set; }
+    }
+    public class Datasets
+    {
+        public string label { get; set; }
+        public string[] backgroundColor { get; set; }
+        public string[] borderColor { get; set; }
+        public string borderWidth { get; set; }
+        public int[] data { get; set; }
+    }
+}
---
+++ u19009209_HW06/Models/ViewModels/customProduct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u19009209_HW06.Models.ViewModels
+{
+    public class customProduct
+    {
+        public string productName { get; set; }
+        public string brandName { get; set; }
+    }
+}
